1c02253f90389397af24b8594d04a1e65c54f242
simmontali <sim.montali@gmail.com>
2020-04-08 12:23:31 +0200

    Testing responses

--- __init__.py
+++ __init__.py
@@ -7,9 +7,11 @@ class HospitalTriage(MycroftSkill):
 
     @intent_file_handler('triage.hospital.intent')
     def handle_triage_hospital(self, message):
-        self.speak_dialog('triage.hospital')
+        reply = self.get_response(dialog='Quali sono i sintomi?', data=None, validator=None,
+                                  on_fail=None, num_retries=-1)
+        # self.speak_dialog('triage.hospital')
+        self.speak_dialog(reply)
 
 
 def create_skill():
     return HospitalTriage()
-
