771722b8a9e168dd2d4c5d59f1a9116326ef91ef
simmontali <sim.montali@gmail.com>
2020-04-09 16:50:53 +0200

    Added faints, bleeding, shock, cough, fractures, and COVID questionnaire

--- __init__.py
+++ __init__.py
@@ -1,10 +1,12 @@
 from mycroft import MycroftSkill, intent_file_handler
 
 
-# TODO: define some decorators:
-#   covid symptom -> takes you to covid questions
-#   symptom -> asks you about pain and other symptoms
-#   When using them, put the general symptom before of the covid one, so that covid questions get asked first.
+# TODO:
+# 1. define some decorators:
+#       covid symptom -> takes you to covid questions
+#       symptom -> asks you about pain and other symptoms
+#       When using them, put the general symptom before of the covid one, so that covid questions get asked first.
+# 2. create personal informations request method. add spelling too.
 
 
 class HospitalTriage(MycroftSkill):
@@ -14,18 +16,24 @@ class HospitalTriage(MycroftSkill):
 
     def symptom_handler(handler):
         def ask_about_symptoms(*args, **kwargs):
-            handler(*args, **kwargs)
+            returned = handler(*args, **kwargs)
+            args[0].med_record["symptom_declaration"] = args[1].data["utterance"]
             # I'm using args[0] here instead of self, but it works the same
             args[0].request_other_symptoms()
             args[0].evaluate_pain()
+            return returned
         return ask_about_symptoms
 
     def covid_symptom(handler):
         def check_if_covid(*args, **kwargs):
-            handler(*args, **kwargs)
+            returned = handler(*args, **kwargs)
             args[0].ask_covid_questions()
+            return returned
         return check_if_covid
 
+    # ------------------------------------
+    # TRIAGE START HANDLER
+    # ------------------------------------
     @intent_file_handler('triage.hospital.intent')
     def handle_triage_hospital(self, message):
         # STEP 1A: check if the patient walked in. If so, it is likely that he's not urgent.
@@ -55,9 +63,37 @@ class HospitalTriage(MycroftSkill):
         self.med_record["code"] = 'red'
         self.speak_dialog('symptoms.bleeding')
 
+    # SHOCK
+    @intent_file_handler('symptoms.shock.intent')
+    @symptom_handler
+    def handle_shock(self, message):
+        self.med_record["main_symptom"] = "shock"
+        self.med_record["code"] = "red"
+        self.speak_dialog('symptoms.shock')
+
+    # BREATH
+    @intent_file_handler('symptoms.breath.intent')
+    @symptom_handler
+    @covid_symptom
+    def handle_breathing(self, message):
+        self.med_record["main_symptom"] = "breathing"
+        self.med_record["code"] = "red"
+        self.speak_dialog('symptoms.breath')
+
+    @intent_file_handler('symptoms.fracture.intent')
+    @symptom_handler
+    def handle_fracture(self, message):
+        self.med_record["main_symptom"] = "fracture"
+        self.log.info(message.data)
+        self.med_record["limb"] = message.data.get('limb')
+        did_i_get_that = self.ask_yesno(
+            'symptoms.fracture', {"article": message.data.get('article'), "limb": message.data.get('limb')})
+        self.med_record["code"] = "yellow"
+        if did_i_get_that == 'no':
+            self.speak_dialog('main_symptom', expect_response=True)
     # ------------------------------------
     # HELPERS
-    #   This are being used in the decorators.
+    #   These are being used in the decorators.
     # ------------------------------------
 
     def request_other_symptoms(self):
@@ -75,10 +111,38 @@ class HospitalTriage(MycroftSkill):
         # This check is needed because of the overlapping between 6 and the "essere" verb
         if reply == 'sei':
             reply = 6
-        self.log.info(reply)
+        self.med_record["pain_index"] = reply
 
     def ask_covid_questions(self):
-        self.med_record["covid_index"] = self.ask_yesno('Hai il covid?')
+        self.speak_dialog('gotta_check_covid')
+        covid_score = 1
+        # Let's first check if the patient knows his temperature
+        has_checked_fever = self.ask_yesno('has_checked_fever')
+        self.log.info(has_checked_fever)
+        # If he/she has a fever, it may be a COVID infect
+        if has_checked_fever == "yes":
+            temperature_string = self.get_response(dialog='get_temperature',
+                                                   data=None, validator=None, on_fail=None, num_retries=-1)
+            self.med_record["fever"] = extract_temperature(temperature_string)
+            if self.med_record["fever"] > 37.5:
+                covid_score = 10
+
+        # Let's define an array of tuples, each containing the yes/no question string and its COVID index multiplier
+        yesno_questions = [("has_sore_throat", 1.3), ("has_cold", 1.3), ("has_breathing_difficulties",
+                                                                         1.6), ("has_cough", 1.6), ("has_had_contacts", 2), ("misses_taste", 1.7)]
+        self.speak_dialog('will_ask_yesno')
+        # Check if he/she has COVID-compatible symptoms
+        for question in yesno_questions:
+            self.med_record[question[0]] = self.ask_yesno(question[0])
+            if self.med_record[question[0]] == 'yes':
+                covid_score = covid_score * question[1]
+            self.log.info(covid_score)
+
+        self.med_record["covid_score"] = covid_score
+        if covid_score > 40:
+            self.speak_dialog('probably_has_covid')
+        else:
+            self.speak_dialog('doesnt_have_covid')
         self.log.info(self.med_record)
 
 
@@ -91,6 +155,26 @@ def number_validator(utterance):
         return (0 < int(utterance) <= 10)
     except ValueError:
         return False
+
+
+def fever_validator(utterance):
+    if len(utterance) != 3:
+        return False
+    temperature = extract_temperature(utterance)
+    # I guess you're pretty dead if your temperature is 32 or 45, but you never know
+    return 32 <= temperature <= 45
+
+
+def extract_temperature(utterance):
+    # Beware: the ' e ' has to be before the simple space!
+    possible_separators = ['/', '.', ',', ' e ', ' ']
+    for separator in possible_separators:
+        if separator in utterance:
+            temperature_strings = utterance.split(separator)
+            temperature = int(
+                temperature_strings[0])+float(temperature_strings[1])*0.1
+            return temperature
+    return None
 
 
 def create_skill():
